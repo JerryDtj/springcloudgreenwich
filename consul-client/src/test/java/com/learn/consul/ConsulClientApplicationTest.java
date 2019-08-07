b73795d89269cda2adbeee3ac6ff80d162efbe95
邓天骄 <jiao_snow@126.com>
2019-08-07 08:11:48 +0800

    consul

---
+++ consul-client/src/test/java/com/learn/consul/ConsulClientApplicationTest.java
@@ -0,0 +1,22 @@
+package com.learn.consul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Jerry
+ * @Date 2019-08-07 07:34
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulClientApplicationTest {
+
+    @Test
+    public void contextLoader(){
+
+    }
+}
