cb8bb9487528d3b3c9e711bc66f7507d6c24b9a0
邓天骄 <jiao_snow@126.com>
2019-08-07 22:29:48 +0800

    consul consul ribbon

--- consul-client/src/main/java/com/learn/consul/ConsulClientApplication.java
+++ consul-client/src/main/java/com/learn/consul/ConsulClientApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Jerry
  * @Date 2019-08-07 07:34
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsulClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsulClientApplication.class,args);
