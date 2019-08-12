3326620c19a4ecbba2f09bfb284fe6eee8fc24d4
邓天骄 <jiao_snow@126.com>
2019-08-12 22:21:37 +0800

    consul consul ribbon

--- consul-consumer-feign/src/main/java/com/learn/consulconsumerfeign/controller/DcConsumer.java
+++ consul-consumer-feign/src/main/java/com/learn/consulconsumerfeign/controller/DcConsumer.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 服务消费者
+ *
  * @author Jerry
  * @Date 2019-08-12 09:04
  */
