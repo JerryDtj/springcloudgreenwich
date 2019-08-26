d65a0b8231aadab13871d339a4fdf406ff061cb7
邓天骄 <jiao_snow@126.com>
2019-08-26 22:03:37 +0800

    config

--- consul-config-server/src/main/java/com/learn/consulconfigserver/ConsulConfigServerApplication.java
+++ consul-config-server/src/main/java/com/learn/consulconfigserver/ConsulConfigServerApplication.java
@@ -2,17 +2,15 @@ package com.learn.consulconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * consule-config
- *
- * @Author: jerry
- * @date: 2019/8/26 14:57
- * @description:
- **/
-
+ * @author Jerry
+ * @Date 2019/8/26 9:07 下午
+ */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsulConfigServerApplication {
     public static void main(String[] args) {
--- consul-config-server/src/test/java/ConsulConfigServerApplicationTest.java
+++
@@ -1,9 +0,0 @@
-/**
- * despaction
- *
- * @Author: jerry
- * @date: 2019/8/26 14:57
- * @description:
- **/
-public class ConsulConfigServerApplicationTest {
-}
